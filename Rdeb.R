b7f2acc8be50705b3bd14b56ed4f6db5b4046240
Frans Thamura <frans@meruvian.com>
2017-05-23 08:13:18 +0700

    Update Rdeb.R

--- Rdeb.R
+++ Rdeb.R
@@ -16,4 +16,5 @@ install.packages("random",repos='http://cran.rstudio.com')
 
 install.packages("dplyr",repos="http://cran.rstudio.com/")
 install.packages("svglite",repos="http://cran.rstudio.com/")
+install.packages('reshape',repos='http://cran.rstudio.com')
 
